0bdad8bf9ff9ee0bb27ba207e5a325c2a8444c80
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2023-12-12 18:40:27 +0530

    adding admin.php alternative

--- action.php
+++ action.php
@@ -1,9 +1,16 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//to use SMTP download github repo from https://github.com/PHPMailer/PHPMailer and add in files and mention path below
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+
 // Retrieve form data
 $name = $_POST['pname'];
 $email = $_POST['pemail'];
 $phone = $_POST['pcontact'];
-
 
 // Database connection settings
 $servername = 'bom1plzcpnl503502';
@@ -23,26 +30,51 @@ if ($conn->connect_error) {
 $sql = "INSERT INTO Contactdetails (User_Name, Email, Phone_No) VALUES ('$name', '$email', '$phone')";
 
 if ($conn->query($sql) === true) {
+    sendEmail($name, $email, $phone);
     echo "We Will Contact You Soon!";
     //to redirect at home page again
     header("refresh: 1; url = https://panameragroup.com/");
-    $content = "You have a new appointment request from : $name \n
+} else {
+    echo "Error: " . $conn->error;
+}
+
+// Close the database connection
+$conn->close();
+
+// Function to send email using PHPMailer
+function sendEmail($name, $email, $phone) {
+    $recipient = "contactus@panameragroup.com";
+    $subject = "You have a new appointment request";
+
+    $content = "You have a new appointment request from: $name \n
                 Appointment Details:\n
                 Name: $name \n
                 Email: $email \n
                 Phone Number: $phone \n
                 Please contact $name at $email or $phone to schedule or confirm the appointment time.\n
                 Thank you";
-    $recipient = "kunalparkar@kccemsr.edu.in";
-    //$ccRecipient = "kunalparkar1234@gmail.com";
-    $mailheader = "From: $email \r\n";
-    // $mailheader .= "CC: $ccRecipient \r\n";
-    $subject = "You have a new appointment request";
-    mail($recipient, $subject, $content, $mailheader) or die("Error!");
-} else {
-    echo "Error: ";
-}
 
-// Close the database connection
-$conn->close();
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->isSMTP();
+        $mail->Host       = 'mail.panameragroup.com';  // Specify the SMTP server
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'contactus@panameragroup.com';    // SMTP username
+        $mail->Password   = 'Mayflower!12';    // SMTP password
+        $mail->SMTPSecure = 'tls';                    // Enable TLS encryption, `ssl` also accepted
+        $mail->Port       = 587;                      // TCP port to connect to
+
+        $mail->setFrom($email, $email);
+        $mail->addAddress($recipient);
+
+        $mail->isHTML(false);
+        $mail->Subject = $subject;
+        $mail->Body    = $content;
+
+        $mail->send();
+    } catch (Exception $e) {
+        echo "Mailer Error: {$mail->ErrorInfo}";
+    }
+}
 ?>
